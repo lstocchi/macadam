ca2f5a24443e1f75d680935a0c521b3b9d1f9cb9
Christophe Fergeau <cfergeau@redhat.com>
2024-06-13 13:54:21 +0200

    build: Add vendorcheck target
    This ensures `vendor` does not get out of sync

---
+++ build-scripts/verify-vendor.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ -n $(git status -s vendor/) ]]; then
+        echo 'vendor/ directory has uncommitted changes, please check `git status vendor`'
+        exit 1
+fi
+
+make vendor
+
+go mod verify
+
+echo "Diffing $(pwd)"
+git diff --exit-code vendor go.mod go.sum
+
+if [[ $? -eq 0 ]]
+then
+  echo "$(pwd) is up to date."
+else
+  echo "$(pwd) is out of date. Please run make vendor"
+  exit 1
+fi
