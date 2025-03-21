e8a56e08f0730296b99a7daf48c09a94d8d0c92a
lstocchi <lstocchi@redhat.com>
2025-03-21 11:56:02 +0100

    make rm action accepts machine name
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- cmd/macadam/rm.go
+++ cmd/macadam/rm.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"github.com/containers/podman/v5/pkg/machine"
 	"github.com/crc-org/macadam/cmd/macadam/registry"
 	macadam "github.com/crc-org/macadam/pkg/machinedriver"
-	"github.com/containers/podman/v5/pkg/machine"
 	"github.com/spf13/cobra"
 )
 
 var (
 	rmCmd = &cobra.Command{
-		Use:     "rm [options]",
+		Use:     "rm [options] [MACHINE]",
 		Short:   "Remove an existing machine",
 		Long:    "Remove a managed virtual machine ",
 		RunE:    rm,
-		Args:    cobra.MaximumNArgs(0),
+		Args:    cobra.MaximumNArgs(1),
 		Example: `macadam rm`,
 	}
 )
@@ -33,7 +33,12 @@ func init() {
 }
 
 func rm(_ *cobra.Command, args []string) error {
-	driver, err := macadam.GetDriverByMachineName(defaultMachineName)
+	machineName := defaultMachineName
+	if len(args) > 0 && len(args[0]) > 0 {
+		machineName = args[0]
+	}
+
+	driver, err := macadam.GetDriverByMachineName(machineName)
 	if err != nil {
 		return nil
 	}
